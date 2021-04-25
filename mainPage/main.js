971c0f9ef5fe0c8d00b88205ed40e563c0227c6f
Anna-Liapina <77729278+Anna-Liapina@users.noreply.github.com>
2021-04-26 02:05:49 +0300

    add animations

--- mainPage/main.js
+++ mainPage/main.js
@@ -1,7 +1,32 @@
 
+    var options = {
+        offset  : '20%',
+        moveX   : '50px',
+        moveY   : '200px',
+      }
 
-$(document).ready(function() {
-    $(".animate__animated").animated("fadeInUp", "fadeOutLeft");
-})
+      var changes1 = {
+        offset  : '15%',
+        rotateX: '90deg',
+        transition: '1s',
+      }
+
+      var changes2 = {
+        offset  : '15%',
+        rotateX: '90deg',
+        transition: '2s',
+      }
+
+      var changes3 = {
+        offset  : '15%',
+        rotateX: '90deg',
+        transition: '3s',
+      }
+
+    $('.featurette').smoove(options);
+
+    $('.shpunk1').smoove(changes1);
+    $('.shpunk2').smoove(changes2);
+    $('.shpunk3').smoove(changes3);
 
 
